a675da21ae01db0918c62c502603e046bb401886
Gil Ben David <giltalgil987@gmail.com>
2019-08-01 20:26:20 +0300

    Add server for live streaming.

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+import cv2
+import sys
+import numpy
+
 
 app = Flask(__name__)
 
 
 # Route for main page
 @app.route('/')
-def hello_world():
+def index():
     return render_template("index.html")
+
+
+def get_frame():
+    camera_port = 0
+    # Making a web cam object
+    camera = cv2.VideoCapture(camera_port)
+
+    while True:
+        im = camera.read()
+        imgencode = cv2.imencode('.jpg',im)[1]
+        stringData = imgencode.tostring()
+        yield (b'--frame\r\n'b'Content-Type: text/plain\r\n\r\n' + stringData + b'\r\n')
+    del(camera)
+
+
+@app.route('/frame')
+def calc():
+    return Response(get_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 # Run the flask server
